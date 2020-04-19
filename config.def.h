be75e3d19a6e5aece5d7dc51c3f236fa68b4f170
Felix Strobel <mail@felixstrobel.de>
2020-04-19 23:10:10 +0200

    add fuzzy patches

--- config.def.h
+++ config.def.h
@@ -1,23 +1,29 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-/* -fn option overrides fonts[0]; default X11 font or font set */
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     *//* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"JetBrainsMono Nerd Font:size=11"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#b0b0b0", "#022222" },
+	[SchemeSel] = { "#5A9A00", "#033333" },
+	[SchemeSelHighlight] = { "#a78904", "#033333" },
+	[SchemeNormHighlight] = { "#a78904", "#022222" },
+	[SchemeOut] = { "#a68904", "#FF0000" },
+	[SchemeMid] = { "#b0b0b0", "#022222" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 20;
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 3;
